dc4a8b2c06d2ee901ec3f3dc4fbdb0e26bb0a6f1
Davronbek96 <davronbekmalikov96@gmail.com>
2025-05-23 22:15:48 +0900

    Data Transformation

--- src/logger.py
+++ src/logger.py
@@ -2,19 +2,20 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Generate a timestamped log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Create logs directory
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
 
+# Full path to the log file
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-
-
+    filemode='a',  # 'a' means append mode
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
-
-if __name__=='__main__':
-    logging.info('Logging started')
