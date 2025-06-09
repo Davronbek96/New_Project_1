22e83efe33abd2eec0716f477c619ad9393ab76a
Davronbek96 <davronbekmalikov96@gmail.com>
2025-06-09 22:40:21 +0900

    Deployment Configuration

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import GridSearchCV
 
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -16,12 +17,14 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
+
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
             return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
+
 
 def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
